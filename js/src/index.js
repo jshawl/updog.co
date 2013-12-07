5d8b339aa4873a5fb60b2ddd94e4ad2b8350e80d
Jesse Shawl <jesse@jshawl.com>
2013-12-06 23:35:28 -0500

    fix js header scroll

--- js/src/index.js
+++ js/src/index.js
@@ -3,14 +3,12 @@
 	var lastScrollTop = 0;
 	$(window).bind('scroll', function(event){
 	    var st = $(this).scrollTop();
-	    if (st % 20 == 0){
-		if (st > lastScrollTop && st > 10){
-		    $('header').addClass('hidden');
-		} else {
-		    $('header').removeClass('hidden');
-		    lastScrollTop = st;
-		}
+	    if (st > lastScrollTop && st > 10){
+		$('header').addClass('hidden');
+	    } else {
+		$('header').removeClass('hidden');
 	    }
+	    lastScrollTop = st;
 	});
     });
 })(jQuery);
