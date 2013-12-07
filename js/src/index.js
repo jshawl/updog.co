3ed98aa9e9fc2e66831317e83e3abf6055884940
Jesse Shawl <jesse@jshawl.com>
2013-12-06 23:22:04 -0500

    fix index

--- js/src/index.js
+++ js/src/index.js
@@ -1,18 +1,16 @@
 ;(function(){
-	$(function(){
+    $(function(){
 	var lastScrollTop = 0;
 	$(window).bind('scroll', function(event){
-	  console.log($(this).scrollTop());
-	  var st = $(this).scrollTop();
-	  if (st % 20 == 0){
-	  if (st > lastScrollTop && st > 10){
-	    $('header').addClass('hidden');
-	 } else {
-	     $('header').removeClass('hidden');
-			      }
-	  lastScrollTop = st;
-	  }
+	    console.log($(this).scrollTop());
+	    var st = $(this).scrollTop();
+	    if (st % 20 == 0){
+		if (st > lastScrollTop && st > 10){
+		    $('header').addClass('hidden');
+		} else {
+		    $('header').removeClass('hidden');
+		    lastScrollTop = st;
+		}
 	});
-
-	});
+    });
 })(jQuery);
