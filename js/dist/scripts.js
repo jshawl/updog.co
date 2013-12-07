c878737334671ee968bc0da4588d0a40788bbfd8
Jesse Shawl <jesse@jshawl.com>
2013-12-06 23:27:35 -0500

    remove console.log

--- js/dist/scripts.js
+++ js/dist/scripts.js
@@ -25,7 +25,6 @@
     $(function(){
 	var lastScrollTop = 0;
 	$(window).bind('scroll', function(event){
-	    console.log($(this).scrollTop());
 	    var st = $(this).scrollTop();
 	    if (st % 20 == 0){
 		if (st > lastScrollTop && st > 10){
